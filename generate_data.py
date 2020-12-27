22dbb5b8ea3776335676af715834ece6473219cb
hanatok <summersnow9403@gmail.com>
2020-12-26 22:26:34 -0600

    Benchmarks against numpy.

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import numpy as np
-import timeit
+import time
 
 # matrix A size
 N = 4000
@@ -14,5 +14,9 @@ B = np.random.rand(N, M)
 np.savetxt('matA.txt', A)
 np.savetxt('matB.txt', B)
 # solve
+start = time.time_ns()
 X = np.linalg.solve(A, B)
+end = time.time_ns()
+elapsed = (end - start) / (10 ** 9)
+print(f'Time: {elapsed} s')
 np.savetxt('reference.txt', X, fmt='%12.7f')
